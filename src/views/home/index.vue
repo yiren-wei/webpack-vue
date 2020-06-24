739aaaf22fc87cfa79b051ee96315c1687393bfd
DAHUATECH\226863 <wei_yiren@dahuatech.com>
2020-06-24 08:52:57 +0800

    初版

---
+++ src/views/home/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{this.message}}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello,Webpack-Vue"
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
